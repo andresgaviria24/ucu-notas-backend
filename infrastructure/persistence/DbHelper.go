2bf9cc7f6f3e951291f18f4a162d43f4441a4e11
Andrés Gaviria <andres_felipe_gaviria28@hotmail.com>
2022-12-09 11:21:37 -0500

    ajuste en conexion

--- infrastructure/persistence/DbHelper.go
+++ infrastructure/persistence/DbHelper.go
@@ -2,8 +2,9 @@ package persistence
 
 import (
 	"fmt"
+	"os"
 	"time"
-	"university/infrastructure/repository"
+	"ucu-notas-backend/infrastructure/repository"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -17,14 +18,13 @@ type DbHelper struct {
 
 func InitOrdersHelper() (*DbHelper, error) {
 
-	/*user := os.Getenv("DB_USER")
+	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
-	name := os.Getenv("DB_NAME")
 
-	config := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)*/
-	config := fmt.Sprintf("root@tcp(127.0.0.1:3306)/sys?charset=utf8mb4&parseTime=True&loc=Local")
+	config := fmt.Sprintf("%s:%s@tcp(%s:%s)/sys?parseTime=true", user, pass, host, port)
+	//config := fmt.Sprintf("root@tcp(127.0.0.1:3306)/sys?charset=utf8mb4&parseTime=True&loc=Local")
 
 	ordersDb, err := gorm.Open(mysql.Open(config),
 		&gorm.Config{Logger: logger.Default.LogMode(logger.Info), SkipDefaultTransaction: true})
